da76f2a5b5c2bd7803530335d4ff1dfa34c29456
Tim Oleson <timothy.oleson@gmail.com>
2020-09-10 11:11:43 -0400

    Code added

---
+++ src/ClientModels/NewSessionModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestingControllersSample.ClientModels
+{
+    #region snippet_HomeController
+   
+        public class NewSessionModel
+        {
+            [Required]
+            public string SessionName { get; set; }
+        }
+    
+    #endregion
+}
